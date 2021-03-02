dc25139c1aa9c4ddabdc126d0e5ce55ed0a8240e
GectoR <dmitry.pustowoy@yandex.ru>
2021-03-02 22:02:19 +0300

    update server.ts

--- 01-nodejs-with-docker-db/server.ts
+++ 01-nodejs-with-docker-db/server.ts
@@ -38,6 +38,34 @@ server.get<{ Querystring: IMagicDbRequest }>(
   }
 );
 
+// READ
+server.get("/news", async (req, reply) => {
+  reply.code(200).send([
+    { tile: "123", text: "text" },
+    { tile: "2000", text: "text 2" },
+  ]);
+});
+
+// CREATE
+server.post("/news", async (req, reply) => {
+  console.log("create with params", req.body);
+  reply.code(200).send({ message: "create ok!", body: req.body });
+});
+
+// UPDATE
+server.put<{ Params: { id: string } }>("/news/:id", async (req, reply) => {
+  console.log("update where id=", req.params.id);
+  reply
+    .code(200)
+    .send({ message: "update ok!", id: req.params.id, body: req.body });
+});
+
+// DELETE
+server.delete<{ Params: { id: string } }>("/news/:id", async (req, reply) => {
+  console.log("delete where id=", req.params.id);
+  reply.code(200).send({ message: "delete where ok!", id: req.params.id });
+});
+
 server.listen(3000, async (err, address) => {
   if (err) {
     console.error(err);
