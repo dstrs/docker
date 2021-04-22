1899783c016f4ab7a5b05eef2df63f31fbabdb0a
GectoR <dmitry.pustowoy@yandex.ru>
2021-04-22 14:21:56 +0300

    add task 2

--- 00-plain-js-tasks/README.md
+++ 00-plain-js-tasks/README.md
@@ -1,9 +1,14 @@
+# task00
 ```
 node --require ts-node/register tasks/00-table-parser/00-table-parser.ts
 ```
-
-или 
-
+или
 ```
 npx nodemon tasks/00-table-parser/00-table-parser.ts
+```
+
+# task01
+
+```
+npx ts-node-dev tasks/01-airbnb-json-parser/01-airbnb-json-parser.ts
 ```
---
+++ 00-plain-js-tasks/tasks/01-airbnb-json-parser/01-airbnb-json-parser.ts
@@ -0,0 +1,13 @@
+let input1 = `./tasks/01-airbnb-json-parser/response`;
+
+function solution(firstArg: string) {
+  console.log("Go!");
+  return [];
+}
+
+solution(input1);
+
+// assert.deepStrictEqual(solution(input1), [
+//   [2020, "mercedes-benz", "a-class"],
+//   [1997, "Ford", "E350"],
+// ]);
